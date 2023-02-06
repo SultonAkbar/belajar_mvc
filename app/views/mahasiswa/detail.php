5f40f98377e040601143492ef4a9dff4af9f8795
SultonAkbar <sultonakbar24@gmail.com>
2023-02-06 19:19:47 +0800

    done-error

--- app/views/mahasiswa/detail.php
+++ app/views/mahasiswa/detail.php
@@ -1,12 +1,13 @@
 <div class="container mt-5">
     <h1 class="mb-5">Detail <?= $data['mhs']['nama']; ?></h1>
     <div class="card" style="width: 20rem;">
-        <img src="<?= $data['mhs']['gambar']; ?>" class="card-img-top" alt="...">
+        <img src="<?= BASEURL; ?>img/<?= $data['mhs']['gambar']; ?>" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title"><?= $data['mhs']['nama']; ?></h5>
-            <h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['NIS']; ?></h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" style="text-decoration: none;" class="badge text-bg-secondary">Kembali</a>
+            <h6 class="card-subtitle text-muted"><?= $data['mhs']['NIS']; ?></h6>
+            <p class="card-text mt-3">Email : <?= $data['mhs']['email']; ?></p>
+            <p class="card-text">Jurusan : <?= $data['mhs']['jurusan']; ?></p>
+            <a href="<?= BASEURL; ?>mahasiswa" style="text-decoration: none;" class="badge text-bg-secondary">Kembali</a>
         </div>
     </div>
 </div>
